1a005cfc332dcc8c5012d556878cb671a6bc5135
frenchllama <113678232+frenchllama@users.noreply.github.com>
2023-09-21 17:01:37 +0530

    Add files via upload

--- 10.13.3.26/codes/code.py
+++ 10.13.3.26/codes/code.py
@@ -1,27 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def diceRoll():
-    dice = np.array(np.random.randint(1, 7, size = 1000000))
-    return dice
+#sample size
+simlen = 10000
+#Possible outcomes
+n = range(-5,6)
+# Generate X1 and X2
+y1 = np.random.randint(1,7, size=(1, simlen))
+y2 = np.random.randint(1,7, size=(1, simlen))
+#Generate X
+X = np.subtract(y1,y2)
+#Find the frequency of each outcome
+unique, counts = np.unique(X, return_counts=True)
+#print the probability
+print("the possible difference values are:", unique)
+print("the probabiliteis of these difference values are:", counts/simlen)
+prob = counts/simlen
 
-X = diceRoll()
-Y = diceRoll()
+#Theoretical probability
+pArray = [1/36, 1/18, 1/12, 1/9, 5/36, 1/6, 5/36, 1/9, 1/12, 1/18, 1/36]
 
-Z = X-Y
-
-z_Vals, z_Freq = np.unique(Z, return_counts = True)
-z_Freq = z_Freq/1000000
-
-plt.figure(figsize=(8, 6))
-plt.stem(np.unique(Z), z_Freq, markerfmt='ro', basefmt=' ', linefmt='k-')
-plt.title('Stem Plot of Z = X - Y ')
-plt.xlabel('Z (Difference of X and Y)')
-plt.ylabel('Probability')
-plt.xticks(np.arange(-5, 6))
-plt.grid(True)
-plt.savefig('../figs/Z.png')
-
-print(z_Vals, ' , ', z_Freq)
-
-
+#Plotting
+plt.stem(n,pArray, markerfmt='bo', linefmt='b-',use_line_collection=True)
+plt.stem(n,prob, markerfmt='ro', linefmt='g',use_line_collection=True)
+plt.xlabel('$n$')
+plt.ylabel('$p_{Z}(n)$')
+plt.title("Theoretical vs Simulated")
+plt.legend(['Theoretical', 'Simulated'])
+plt.savefig("../figs/z.png",bbox_inches='tight')
